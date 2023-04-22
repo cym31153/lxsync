37575cf971b9e8a93ac97778c037e5f4e94b39ca
cym31153 <63576755+cym31153@users.noreply.github.com>
2023-04-22 22:00:00 +0800

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,15 +11,15 @@ module.exports = {
       watch: false,
       ignore_watch: ['node_modules', 'logs', 'data'],
       env: {
-        // PORT: 3100,
+        PORT: 9527,
         NODE_ENV: 'development',
       },
       env_production: {
-        // PORT: 3100, // 配置绑定的端口号默认9527
-        // BIND_IP: '0.0.0.0', // 配置绑定IP为0.0.0.0将接受所有IP访问
-        // CONFIG_PATH: '',
-        // LOG_PATH: '',
-        // DATA_PATH: '',
+         PORT: 9527, // 配置绑定的端口号默认9527
+         BIND_IP: '0.0.0.0', // 配置绑定IP为0.0.0.0将接受所有IP访问
+        CONFIG_PATH: '1',
+         LOG_PATH: '2',
+        DATA_PATH: '3',
         NODE_ENV: 'production',
       },
     },
